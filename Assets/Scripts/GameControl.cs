b5bc55bc072e898a73a9961b7d74e6b1c11b5b0b
LIU-YiFeng-1 <liuyifeng.smc@gmail.com>
2022-03-07 17:22:06 +0800

    update player shield effect + hp logic
    but not working proper for the shield icon and player hp

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 public class GameControl : MonoBehaviour
 {
     public GameObject arCamera;
-    public GameObject shield;
     public Image oppHp;
     public Image playerHp;
-    public Image shieldHp;    
+
     //following is for Hp
     public int maxHp = 100;
     private int currentOppHp;
@@ -19,23 +18,45 @@ public class GameControl : MonoBehaviour
     public GameObject grenadePrefab;
     public float throwForce = 40f;
     private int grenadeDamage = 30;
-    //following is for shield
+    //following is for general shield status
+    private int maxShieldHp = 30;
+    private float shieldCountDownValue = 10.0f; 
+    //following is for opponent shield
+    
+    public GameObject oppShield;
+    public Image oppShieldHp;
+    public float oppShieldCountDown; 
     public GameObject oppShield1;
     public GameObject oppShield2;
     public GameObject oppShield3;
-    private int shieldCounter = 3;
-    private int maxShieldHp = 30;
-    private int currentShieldHp;
-    private bool isShieldActive;
-    public float shieldCountDown = 10.0f; //this is to be updated later to 10sec, 3sec is for testing purpose
+    private int oppShieldCounter = 3;
+    private int currentOppShieldHp;
+    private bool isOppShieldActive;
+
+    //following is for player shield
+    public GameObject playerShield;
+    public Image playerShieldHp;
+    public float playerShieldCountDown; 
+    public GameObject playerShield1;
+    public GameObject playerShield2;
+    public GameObject playerShield3;
+    private int playerShieldCounter = 3;
+    private int currentPlayerShieldHp;
+    private bool isPlayerShieldActive;
     // Start is called before the first frame update
     void Start()
     {
         currentPlayerHp = maxHp;
         currentOppHp = maxHp;
-        currentShieldHp = maxShieldHp;
-        shield.SetActive(false); //set false if using shield button; set true if testing for shield hp
-        isShieldActive = false;
+        currentPlayerShieldHp = maxShieldHp;
+        currentOppShieldHp = maxShieldHp;
+        playerShieldCountDown = shieldCountDownValue;
+        oppShieldCountDown = shieldCountDownValue;
+
+        playerShield.SetActive(false);
+        isPlayerShieldActive = false;
+        oppShield.SetActive(false); //set false if using shield button; set true if testing for shield hp
+        isOppShieldActive = false;
     }
 
     // Update is called once per frame
@@ -43,31 +64,76 @@ public class GameControl : MonoBehaviour
     {
         OpponentShieldAction();
     }
-
-    public void ActivateOpponentSheild()
+    public void ActivatePlayerSheild()
     {
-        Debug.Log("shield button clicked! but outside if statement");
-        if(shieldCounter > 0)
+        Debug.Log("ActivatePlayerSheild function called! but outside if statement");
+        if(playerShieldCounter > 0)
         {
-            shield.SetActive(true);
-            isShieldActive = true;
-            shieldHp.fillAmount = 1;    
-            currentShieldHp = maxShieldHp;
-            shieldCounter--;
-            Debug.Log("shield count -1; inside if statement");
+            playerShield.SetActive(true);
+            isPlayerShieldActive = true;
+            playerShieldHp.fillAmount = 1;    
+            currentPlayerShieldHp = maxShieldHp;
+            playerShieldCounter--;
+            Debug.Log("player shield count -1; inside if statement");
         }
     }
-    public void TakeBulletDamage() 
+    public void ActivateOpponentSheild()
+    {
+        Debug.Log("ActivateOpponentSheild function called! but outside if statement");
+        if(oppShieldCounter > 0)
+        {
+            oppShield.SetActive(true);
+            isOppShieldActive = true;
+            oppShieldHp.fillAmount = 1;    
+            currentOppShieldHp = maxShieldHp;
+            oppShieldCounter--;
+            Debug.Log("opponent shield count -1; inside if statement");
+        }
+    }
+
+    //player takes damage based on the input signal from external comm only, disable ray cast
+    //only there is a hit registered, then decrement the playerHp
+    public void PlayerTakeBulletDamage() 
+    {   
+        Debug.Log("opponent fired, gun is being pressed, player gets hit");
+
+        if(isPlayerShieldActive)
+        {
+                currentPlayerShieldHp -= bulletDamage;
+                playerShieldHp.fillAmount = (float)currentPlayerShieldHp / (float)maxShieldHp;           
+                Debug.Log("player gets hit but shield is up; update playerShieldHp accordingly");
+        } else
+        {
+            currentPlayerHp -= bulletDamage;
+            playerHp.fillAmount = (float)currentPlayerHp / (float)maxHp;           
+            Debug.Log("player is hit and is withou shield; update playerHp accordingly");
+        }
+
+        //actions for shield
+        if(currentPlayerShieldHp==0) {
+            playerShield.SetActive(false);
+            isPlayerShieldActive = false;
+            currentPlayerShieldHp = maxShieldHp;
+            Debug.Log("shield is down; shield is down due to damage taken!");
+        }
+
+        if(currentPlayerHp==0) {
+            Debug.Log("player died!");
+        }
+    }
+    
+
+    public void OpponentTakeBulletDamage() 
     {   
         RaycastHit hit;
-        Debug.Log("player fired, gun is being pressed");
+        Debug.Log("player fired, gun is being pressed, opponent gets hit");
 
-        if(isShieldActive)
+        if(isOppShieldActive)
         {
             if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
             {
-                currentShieldHp -= bulletDamage;
-                shieldHp.fillAmount = (float)currentShieldHp / (float)maxShieldHp;           
+                currentOppShieldHp -= bulletDamage;
+                oppShieldHp.fillAmount = (float)currentOppShieldHp / (float)maxShieldHp;           
                 Debug.Log("Raycast hit shield!");
             }
         } else
@@ -78,10 +144,10 @@ public class GameControl : MonoBehaviour
         }
 
         //actions for shield
-        if(currentShieldHp==0) {
-            shield.SetActive(false);
-            isShieldActive = false;
-            currentShieldHp = maxShieldHp;
+        if(currentOppShieldHp==0) {
+            oppShield.SetActive(false);
+            isOppShieldActive = false;
+            currentOppShieldHp = maxShieldHp;
             Debug.Log("shield is down; shield is down due to damage taken!");
         }
 
@@ -90,37 +156,74 @@ public class GameControl : MonoBehaviour
         }
     }
     
-    private void OpponentShieldAction()
+    private void PlayerShieldAction()
     {
         //when the shield is activated, the count down timer for the shield is initialized
-        if(isShieldActive) 
+        if(isPlayerShieldActive) 
         {
-            shieldCountDown -= Time.deltaTime;
-            if(shieldCountDown <= 0)
+            playerShieldCountDown -= Time.deltaTime;
+            if(playerShieldCountDown <= 0)
             {
-                shield.SetActive(false);
-                isShieldActive = false;
-                shieldCountDown = 5.0f;
+                playerShield.SetActive(false);
+                isPlayerShieldActive = false;
+                playerShieldCountDown = shieldCountDownValue;
             }
         }
         //this switch statement checks for the shield counter and update the UI accordingly
         //hides the corresponding shield from the status panel
-        switch (shieldCounter)
+        switch (playerShieldCounter)
+        {
+        case 2:
+            playerShield3.SetActive(false);
+            Debug.Log("player Shield number 3 is gone!");
+            Debug.Log("current player shieldCounter =" + playerShieldCounter);
+            break;
+        case 1:
+            playerShield2.SetActive(false);
+            Debug.Log("player Shield number 2 is gone!");
+            Debug.Log("current player shieldCounter =" + playerShieldCounter);
+            break;
+        case 0:
+            playerShield1.SetActive(false);
+            Debug.Log("player Shield number 1 is gone!");
+            Debug.Log("current player shieldCounter =" + playerShieldCounter);
+            break;
+        default:
+            break;
+        }
+        Debug.Log("ShieldAction function called");
+    }
+    private void OpponentShieldAction()
+    {
+        //when the shield is activated, the count down timer for the shield is initialized
+        if(isOppShieldActive) 
+        {
+            oppShieldCountDown -= Time.deltaTime;
+            if(oppShieldCountDown <= 0)
+            {
+                oppShield.SetActive(false);
+                isOppShieldActive = false;
+                oppShieldCountDown = shieldCountDownValue;
+            }
+        }
+        //this switch statement checks for the shield counter and update the UI accordingly
+        //hides the corresponding shield from the status panel
+        switch (oppShieldCounter)
         {
         case 2:
             oppShield3.SetActive(false);
-            Debug.Log("Shield number 3 is gone!");
-            Debug.Log("current shieldCounter =" + shieldCounter);
+            Debug.Log("opponent Shield number 3 is gone!");
+            Debug.Log("current opponent shieldCounter =" + oppShieldCounter);
             break;
         case 1:
             oppShield2.SetActive(false);
-            Debug.Log("Shield number 2 is gone!");
-            Debug.Log("current shieldCounter =" + shieldCounter);
+            Debug.Log("opponent Shield number 2 is gone!");
+            Debug.Log("current opponent shieldCounter =" + oppShieldCounter);
             break;
         case 0:
             oppShield1.SetActive(false);
-            Debug.Log("Shield number 1 is gone!");
-            Debug.Log("current shieldCounter =" + shieldCounter);
+            Debug.Log("opponent Shield number 1 is gone!");
+            Debug.Log("current opponent shieldCounter =" + oppShieldCounter);
             break;
         default:
             break;
